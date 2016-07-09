a99ab8edff44d7debb55683ffcac9631a60f4a14
Akihito Fujiwara <akitoha1207@gmail.com>
2016-07-09 09:51:11 +0900

    Did.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
-ruby "2.1.1"
+ruby "2.2.2"
 
 gem "rspec", "~> 2.14"
+gem "lambda_driver", "~> 1.2.4"
+gem "kansuu", "~> 0.0.0"
+
--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,19 @@
-class BonusDrink
-  def self.total_count_for(amount)
-    0
+require 'lambda_driver'
+require 'kansuu'
+
+module BonusDrink
+  extend Kansuu
+  
+  def bonus(n)
+    n < 3 ? 0
+    : (n / 3) + (bonus n / 3 + n % 3)
   end
+
+  def total_count_for(n)
+    n + (bonus n)
+  end
+
+  map[_.module_function] < instance_methods
 end
+
+#p BonusDrink.total_count_for 100 # => 149
